fca5ba32f7d290eb172c85e1a541af49f5c8c4c4
Morgan Styers <morganstyers@gmail.com>
2019-10-26 12:24:47 -0400

    'is this workin'

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+    saveExample: function(example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
