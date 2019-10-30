9da0dac68e53788052230907e34dc8d908c4bfa6
Pat Perry <risingphx1@gmail.com>
2019-10-30 18:54:20 -0400

    started the DB implementation

---
+++ models/animal.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  var Animal = sequelize.define("Animal", {
+    type: DataTypes.STRING,
+    name: DataTypes.STRING,
+    color: DataTypes.STRING,
+    size: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    lostFound: DataTypes.STRING,
+    lastSeen: DataTypes.TEXT,
+    contactInfo: DataTypes.TEXT,
+    zip: DataTypes.STRING
+  });
+  return Animal;
+};
