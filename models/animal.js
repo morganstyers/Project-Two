30e0c4aeef0540c4edac5a2ae3779445f8c6d7f8
Toney Higgins <toneyhiggins2@gmail.com>
2019-10-30 20:41:25 -0400

    adding schema

--- models/animal.js
+++ models/animal.js
@@ -1,14 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
-  var Animal = sequelize.define("Animal", {
-    type: DataTypes.STRING,
-    name: DataTypes.STRING,
-    color: DataTypes.STRING,
-    size: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    lostFound: DataTypes.STRING,
-    lastSeen: DataTypes.TEXT,
-    contactInfo: DataTypes.TEXT,
-    zip: DataTypes.STRING
-  });
+  var Animal = sequelize.define(
+    "Animal",
+    {
+      type: DataTypes.STRING,
+      name: DataTypes.STRING,
+      color: DataTypes.STRING,
+      size: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      lostFound: DataTypes.STRING,
+      lastSeen: DataTypes.TEXT,
+      contactInfo: DataTypes.TEXT,
+      zip: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
   return Animal;
 };
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS Animal_DB;
-CREATE DATABASE Animal_DB;
+DROP DATABASE IF EXISTS animal_db;
+CREATE DATABASE animal_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+
+
+INSERT INTO Animal (type, name, color, size, description, lostFound, lastSeen, contactInfo, zip)
+VALUES ("dog","fido", "brown", "S", "Brown dog w/spots", "Lost", "Yesterday on Independence", "Pam", "704-555-6166", "28215"),
+("cat", "pixie", "orange", "S", "Orange cat similar to garfield", "Found", "Beatties Ford Rd", "Tom", "704-565-7865", "28214"),
+("bunny", "bugs", "grey", "M", "Plump with rosy red cheeks", "Lost", "Freedom Park", "Sandy", "704-565-9837", "28202"),
+("dog", "dallas", "grey", "M", "Pitbull with blue eyes", "Found", "NODA", "Phil", "704-875-7346", "28264"),
+("turtle", "crush", "green", "S", "Looks like turtle from Finding Nemo", "Lost", "Hopewell High School", "Stacy", "704-637-7865", "28216"),
+("cat", "smokey", "black", "S", "Black cat with grey highlights and yellow eyes", "Found", "Back alley near Food Lion", "Kate", "704-535-7825", "28283"),
+("parrot", "eddie", "red", "S", "Red parrot with rainbow colored wings RARE BREED", "Lost", "Exotic Animals Convention", "Tyler", "704-874-0837", "28214"),
+("dog", "killer", "black", "S", "weiner dog with brown feet", "Found", "Near First Ward Park", "Kim", "704-837-3425", "28235"),
+("cat", "pixie", "orange", "S", "Orange cat similar to garfield", "Lost", "Beatties Ford Rd", "Tom", "704-565-7865", "28214"),
+("cat", "pixie", "orange", "S", "Orange cat similar to garfield", "Found", "Beatties Ford Rd", "Tom", "704-565-7865", "28214")
+
