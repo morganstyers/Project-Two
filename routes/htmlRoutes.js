b5090569aaae248fd3ef785b8d60134349971cec
Pat Perry <risingphx1@gmail.com>
2019-11-02 12:28:08 -0400

    updated some js files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,16 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    db.Animal.findAll({}).then(function(dbAnimals) {
+      res.render("catalogue", {
+        animals: dbAnimals
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+/*   // Load example page and pass in an example by id
+  app.get("/catalogue/, function(req, res) {
+    db.Example.findAll({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.render("example", {
@@ -21,7 +20,7 @@ module.exports = function(app) {
       });
     });
   });
-
+ */
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
