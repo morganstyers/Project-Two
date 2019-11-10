f2d4bb38cbeb0e9e71230d6c9bdbb926aec5bd1b
Morgan Styers <morganstyers@gmail.com>
2019-11-10 02:27:13 -0500

    database and heroku both work now

--- server.js
+++ server.js
@@ -24,7 +24,24 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+//make connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "gammaHOTEL911",
+    database: "animal_db"
+  });
+}
+connection.connect();
+// Export connection for our ORM to use.
+module.exports = connection;
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
